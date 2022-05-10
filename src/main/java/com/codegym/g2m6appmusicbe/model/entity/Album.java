3f2b0ea22bfd6f270f07bc33d3728b506b312c97
kieuhuan <kieuhuanbka@gmail.com>
2022-05-11 00:30:37 +0700

    add model

--- src/main/java/com/codegym/g2m6appmusicbe/model/entity/Album.java
+++ src/main/java/com/codegym/g2m6appmusicbe/model/entity/Album.java
@@ -1,4 +1,4 @@
-package model.entity;
+package com.codegym.g2m6appmusicbe.model.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -7,11 +7,11 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "categories")
+@Table(name = "albums")
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class Category {
+public class Album {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
