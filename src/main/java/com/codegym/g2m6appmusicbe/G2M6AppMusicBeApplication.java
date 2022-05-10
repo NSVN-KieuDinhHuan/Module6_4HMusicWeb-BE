48d82c579d5b9fc8f36711ee4dee66ebbf2e5723
kieuhuan <kieuhuanbka@gmail.com>
2022-05-10 08:58:44 +0700

    first commit

---
+++ src/main/java/com/codegym/g2m6appmusicbe/G2M6AppMusicBeApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.g2m6appmusicbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G2M6AppMusicBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G2M6AppMusicBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codegym/g2m6appmusicbe/G2M6AppMusicBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.g2m6appmusicbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G2M6AppMusicBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
