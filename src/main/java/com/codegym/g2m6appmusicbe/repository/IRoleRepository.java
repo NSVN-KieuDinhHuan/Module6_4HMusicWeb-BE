8eceeee5476c2956ad0664eb9980be0585baa582
Hungyuuki <hungyuuki777@gmail.com>
2022-05-11 10:53:27 +0700

    addRepo,Service,model

---
+++ src/main/java/com/codegym/g2m6appmusicbe/repository/IRoleRepository.java
@@ -0,0 +1,7 @@
+package com.codegym.g2m6appmusicbe.repository;
+
+import com.codegym.g2m6appmusicbe.model.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IRoleRepository extends JpaRepository<Role, Long> {
+}
---
+++ src/main/java/com/codegym/g2m6appmusicbe/service/user/IUserService.java
@@ -0,0 +1,8 @@
+package com.codegym.g2m6appmusicbe.service.user;
+
+import com.codegym.g2m6appmusicbe.model.entity.User;
+import com.codegym.g2m6appmusicbe.service.IGeneralService;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface IUserService extends IGeneralService<User>, UserDetailsService {
+}
